6fbc4bc292640654d185e4d465aa50f9b106d87d
isaac <kaueisaac769@gmail.com>
2025-08-22 22:38:26 -0300

    apredendo api

---
+++ app/Http/Controllers/PrimeiraApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\primeira_api;
+use Illuminate\Http\Request;
+
+class PrimeiraApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(primeira_api $primeira_api)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, primeira_api $primeira_api)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(primeira_api $primeira_api)
+    {
+        //
+    }
+}
---
+++ app/Models/primeira_api.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class primeira_api extends Model
+{
+    protected $fillbale = [
+        'name',
+        'preco',
+        'marca'
+    ];
+}
