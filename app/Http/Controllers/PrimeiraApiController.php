350704afd0762de881bf2e8a1f828d45e7406778
isaac <kaueisaac769@gmail.com>
2025-08-26 01:30:22 -0300

    Add initial .env configuration and update PrimeiraApiController methods for product management

--- app/Http/Controllers/PrimeiraApiController.php
+++ app/Http/Controllers/PrimeiraApiController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\primeira_api;
 use Illuminate\Http\Request;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
 class PrimeiraApiController extends Controller
 {
     /**
@@ -12,7 +16,23 @@ class PrimeiraApiController extends Controller
      */
     public function index()
     {
-        //
+        $registros = Produtos::all();
+
+        $contador = $registros->count();
+
+        if ($contador > 0){
+            return response()->json([
+                'success' => true,
+                'message' => 'Produtos encontradas com sucesso!',
+                'data' => $registros,
+                'total' => $contador
+            ], 200); 
+        } else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Nenhum produto encontrada.',
+            ], 404);
+        }
     }
 
     /**
@@ -20,30 +40,109 @@ class PrimeiraApiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validador = Validator::make($request->all(), [
+            'name' => 'required',
+            'marca' => 'required',
+            'preco' => 'required',
+        ]);
+        if($validador->fails()){
+            return response()->json([
+                'success' =>false,
+                'message' => 'Produtos cadastrados com sucesso!',
+                'data' => $registros
+
+            ], 201);
+        } else{
+            return response()-json([
+                'success' => false,
+                'message' => 'Erro ao cadastrar um produto'
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(primeira_api $primeira_api)
+    public function show($id)
     {
-        //
+        $registros = Produtos::find($id);
+
+        if($registros){
+            return response()->json([
+                'success' => true,
+                'message' => 'Produto localizada com sucesso!',
+                'data' =>$registros
+            ], 200);
+        } else{
+            return response()->json([
+                'success' =>false,
+                'message' => 'Produto não localizado.',
+            ], 404);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, primeira_api $primeira_api)
+    public function update(Request $request, string $id)
     {
-        //
+        $validador = Validador::make($request->all(), [
+            'name' => 'required',
+            'message' => 'required',
+            'preco' => 'required',
+        ]);
+
+        if($validador->fails()){
+            return response()->json([
+                'succcess' => false,
+                'message' => 'Registros inválidos',
+                'errors' => $validador->errors()
+            ], 400);
+        }
+
+        $registrosBanco->name = $request->name;
+        $registrosBanco->marca = $request->marca;
+        $registrosBanco->preco = $request->preco;
+
+        if($registrosBanco->save()){
+            return response()->json([
+                'success' => true,
+                'message' => 'Produto atualizado com sucesso!',
+                'data' => $registrosBanco
+            ], 200);
+        } else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao atualizar o produto',
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(primeira_api $primeira_api)
+    public function destroy($id)
     {
-        //
+        $registros = Produtos::find($id);
+
+        if(!$registros){
+            return response()->json([
+                'success' => false,
+                'message' => 'Produto não encontrado'
+            ], 404);
+        }
+
+        if ($registros->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Produto deletado com sucesso'
+            ], 200);
+        }
+
+        return response()->json([
+                'success' => false,
+                'message' => 'Erro ao deletar um produto'
+            ], 500);
+
     }
 }
